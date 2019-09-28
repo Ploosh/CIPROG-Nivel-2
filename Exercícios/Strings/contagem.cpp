c12df97354519a830f850e1bb6237a3f89444483
Fernanda <nandadks@gmail.com>
2019-09-28 08:59:15 -0300

    .

--- Exercícios/Strings/contagem.cpp
+++ Exercícios/Strings/contagem.cpp
@@ -1,8 +1,32 @@
 // conta as vogais de uma frase
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
-    
+    string frase;
+    int a=0,e=0,i=0,o=0,u=0;
+
+    cout << "Insira a frase a ser analisada: ";
+    getline (cin, frase);
+
+    for (int j=0; j<frase.size(); j++){
+        if (frase[j]=='a' || frase[j]=='A' || frase[j]==){
+            a++;
+        }
+        else if (frase[j]=='e' || frase[j]=='E'){
+            e++;
+        }
+        else if (frase[j]=='i' || frase[j]=='I'){
+            i++;
+        }
+        else if (frase[j]=='o' || frase[j]=='O'){
+            o++;
+        }
+        else if (frase[j]=='u' || frase[j]=='U'){
+            u++;
+        }
+    }
+    cout << "A - " << a << "; E - " << e << "; I - " << i << "; O - " << o << "; U - " << u << endl;
 }
