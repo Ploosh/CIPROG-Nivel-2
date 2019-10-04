e93e383a5f3cc76612efd0587d23bd765658997e
Domingos Ferreira Viana Neto <dfvneto@gmail.com>
2019-10-04 15:31:04 -0300

    utilizando set precision e fixed

--- Exercícios/Strings/media.cpp
+++ Exercícios/Strings/media.cpp
@@ -1,5 +1,5 @@
 // media identificada
-
+#include <iomanip>
 #include <iostream>
 #include <string>
 using namespace std;
@@ -7,7 +7,7 @@ using namespace std;
 int main(){
     string nome[3], sobrenome[3];
     float n1, n2, n3, media[3];
-
+    std::cout << std::setprecision(2) << std::fixed;
     for (int i=0; i<3; i++){
         cout << "Insira o nome completo do aluno:\n";
         cin >> nome[i];
@@ -16,7 +16,7 @@ int main(){
         cout << "Insira as 3 notas do aluno:\n";
         cin >> n1 >> n2 >> n3;
         media[i] = (n1+n2+n3)/3;
-        cout << media[i] << endl;
+        cout << media[i] <<endl;
     }
    // for (int i=0; i<3; i++){
     //    cout << "A média de " << nome[i] << sobrenome[i] << " é: " << media[i] << endl;
