64071a0c6611f486ce5fad764cc8e79f019ae528
Fernanda <nandadks@gmail.com>
2019-10-04 16:33:16 -0300

    final 04/10

--- Exercícios/Strings/media.cpp
+++ Exercícios/Strings/media.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(){
     int q, n;
-    cout << "Insira a quantidade de alunos e a quantidade de notas, respectivamente: ";
+    cout << "Insira a quantidade de alunos e a quantidade de notas, respectivamente:\n";
     cin >> q >> n;
     cin.ignore();
 
@@ -14,11 +14,13 @@ int main(){
     float notas[n], media[q];
 
     for (int i=0; i<q; i++){
-        cout << "Insira o nome completo do aluno:\n";
+        cout << "\n\nInsira o nome completo do aluno:\n";
         getline(cin, nome[i]);
 
-        cout << "Insira as notas do aluno:\n";
+        media[i]=0;
+
         for (int j=0; j<n; j++){
+            cout << "Insira a " << j+1 <<"ª nota do aluno: ";
             cin >> notas[j];
             media[i] = media[i]+notas[j];
         
@@ -26,6 +28,9 @@ int main(){
         media[i]=media[i]/n;
         cin.ignore();
     }
+    cout << fixed;
+    cout.precision(2);
+
     for (int i=0; i<q; i++){
         cout << nome[i] << "\nMédia: " << media[i] << endl;
     }
