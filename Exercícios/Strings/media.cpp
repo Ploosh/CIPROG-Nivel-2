3e9fd05cb2072e647a02c3dbf9f69dd366bbc3f1
Fernanda Barreto <nandadks@gmail.com>
2019-10-07 20:29:53 -0300

    .

--- Exercícios/Strings/media.cpp
+++ Exercícios/Strings/media.cpp
@@ -5,11 +5,33 @@
 using namespace std;
 
 int main(){
+<<<<<<< Updated upstream
     int q, n;
     cout << "Insira a quantidade de alunos e a quantidade de notas, respectivamente:\n";
     cin >> q >> n;
     cin.ignore();
 
+=======
+<<<<<<< HEAD
+    string nome[3], sobrenome[3];
+    float n1, n2, n3, media[3];
+    std::cout << std::setprecision(2) << std::fixed;
+    for (int i=0; i<3; i++){
+        cout << "Insira o nome completo do aluno:\n";
+        cin >> nome[i];
+        getline(cin, sobrenome[i]);
+
+        cout << "Insira as 3 notas do aluno:\n";
+        cin >> n1 >> n2 >> n3;
+        media[i] = (n1+n2+n3)/3;
+        cout << media[i] <<endl;
+=======
+    int q, n;
+    cout << "Insira a quantidade de alunos e a quantidade de notas, respectivamente:\n";
+    cin >> q >> n;
+    cin.ignore();
+
+>>>>>>> Stashed changes
     string nome[q];
     float notas[n], media[q];
 
@@ -33,6 +55,10 @@ int main(){
 
     for (int i=0; i<q; i++){
         cout << nome[i] << "\nMédia: " << media[i] << endl;
+<<<<<<< Updated upstream
+=======
+>>>>>>> master
+>>>>>>> Stashed changes
     }
 
 }
