1658ba4c7ddc84f9a6761f6e08385fcf14c8489c
Fernanda Barreto <nandadks@gmail.com>
2019-10-25 15:25:29 -0300

    .

--- Exercícios/Funções/bhaskara.cpp
+++ Exercícios/Funções/bhaskara.cpp
@@ -2,21 +2,22 @@
 #include <cmath>
 using namespace std;
 
-float delta (float a, float b, float c){
+void bhaskara (float a, float b, float c){
     float D;
     D =(pow(b, 2)-4*a*c);
     cout << "Delta = " << D << endl;
-    return D;
+
+    if (D>=0){
+        int x1=0, x2=0;
+        x1=((-b)+sqrt(D))/(2*a);
+        x2=((-b)-sqrt(D))/(2*a);
+
+        cout << "São raízes da equação:\nX1 = " << x1 << endl << "X2 = " << x2 << endl;
+    }
+    else{
+        cout << "A equação possui raizes imaginárias, não é possível calcular as raízes da equação\n";
+    }
 }
-
-float bhaskara (float a, float b, float delta){
-    int x1=0, x2=0;
-    x1=((-b)+sqrt(delta))/(2*a);
-    x2=((-b)-sqrt(delta))/(2*a);
-
-    cout << "São raízes da equação:\nX1 = " << x1 << endl << "X2 = " << x2 << endl;
-}
-
 
 int main(){
     float a, b, c, D;
@@ -24,18 +25,16 @@ int main(){
     cout << "Insira o coeficiente de x²: ";
     cin >> a;
 
+    if (a==0){
+        cout << "Não é uma equação do segundo grau\n";
+        return 0;
+    }
+
     cout << "Insira o coeficiente de x: ";
     cin >> b;
 
     cout << "Insira o termo independente, cano não haja coloque 0: ";
     cin >> c;
 
-    D = delta (a, b, c);
-
-    if (delta >= 0){
-        bhaskara (a, b, D);
-    }
-    else {
-        cout << "A equação possui raizes imaginárias, não é possível calcular";
-    }
+    bhaskara (a, b, c);
 }
